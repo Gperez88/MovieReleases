a413e7a706f9209a3d3ff1663f3bded34ac436de
GP89Developers <perezgabriel89@gmail.com>
2015-03-29 23:39:50 -0400

    adapter sync adapter

--- app/src/main/java/com/gperez88/moviereleases/app/activities/DetailActivity.java
+++ app/src/main/java/com/gperez88/moviereleases/app/activities/DetailActivity.java
@@ -1,6 +1,8 @@
 package com.gperez88.moviereleases.app.activities;
 
+import android.annotation.TargetApi;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
@@ -44,5 +46,11 @@ public class DetailActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @TargetApi(Build.VERSION_CODES.JELLY_BEAN)
+    @Override
+    public Intent getParentActivityIntent() {
+        return super.getParentActivityIntent().addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
     }
 }
